7b9d3414495e101c1670375e2c5c61b86ac2362d
zJaron <gravehag@gmail.com>
2023-04-30 09:37:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Artimiti64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
